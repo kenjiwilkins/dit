772acb41ccb683178ef6ba87298822857820cf9c
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-07-27 21:58:11 +0900

    add button

---
+++ src/dit-button.ts
@@ -0,0 +1,129 @@
+import {
+  LitElement,
+  css,
+  unsafeCSS,
+  ReactiveControllerHost,
+  html,
+  PropertyDeclaration,
+  TemplateResult,
+} from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+
+type ButtonType = "primary" | "secondary" | "tertiary";
+
+@customElement("dit-button")
+export class DitButton extends LitElement {
+  static get properties() {
+    return {
+      color: { type: String, reflect: true },
+      disabled: { type: Boolean, reflect: true },
+    };
+  }
+  constructor() {
+    super();
+    this.color = "primary";
+    this.disabled = false;
+  }
+  color: ButtonType = "primary";
+  disabled: boolean = false;
+
+  static get styles() {
+    return css`
+      .dit-button {
+        display: flex;
+        width: 256px;
+        padding: 16px;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+        border-radius: 8px;
+        background-color: #0017c1;
+        border: 1px solid transparent;
+        color: #fff;
+        font-family: Noto Sans JP;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 700;
+        cursor: pointer;
+      }
+      .dit-button.primary {
+        background-color: #0017c1;
+        color: #fff;
+        border: 1px solid transparent;
+      }
+      .dit-button.primary:hover,
+      .dit-button.primary:active {
+        background-color: #000082;
+      }
+      .dit-button.primary:focus {
+        padding: 15px;
+        outline: 2px solid #d18d0f;
+        outline-offset: 2px;
+      }
+      .dit-button.primary:disabled {
+        background-color: #949497;
+        color: #fff;
+        cursor: not-allowed;
+      }
+      .dit-button.secondary {
+        background-color: transparent;
+        color: #0017c1;
+        border: 1px solid #0017c1;
+      }
+      .dit-button.secondary:hover,
+      .dit-button.secondary:active {
+        background-color: #e8f1fe;
+        color: #000082;
+        border: 1px solid #000082;
+      }
+      .dit-button.secondary:focus {
+        padding: 15px;
+        border: 2px solid #d18d0f;
+        outline: none;
+      }
+      .dit-button.secondary:disabled {
+        background-color: transparent;
+        color: #949497;
+        border: 1px solid #949497;
+        cursor: not-allowed;
+      }
+      .dit-button.tertiary {
+        background-color: transparent;
+        color: #0017c1;
+        text-decoration: underline;
+        border: 1px solid transparent;
+      }
+      .dit-button.tertiary:hover,
+      .dit-button.tertiary:active {
+        color: #000082;
+      }
+      .dit-button.tertiary:focus {
+        padding: 15px;
+        border: 2px solid #d18d0f;
+        outline: none;
+      }
+      .dit-button.tertiary:disabled {
+        background-color: transparent;
+        color: #949497;
+      }
+    `;
+  }
+  render() {
+    return html`
+      <button
+        role="button"
+        class="dit-button ${this.color}"
+        ?disabled=${this.disabled}
+      >
+        <slot></slot>
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "dit-button": DitButton;
+  }
+}
