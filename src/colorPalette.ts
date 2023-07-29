7cd6213923d228116cf3039a987e40e1e5b28692
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2023-07-29 09:43:10 +0900

    add: color palette

---
+++ src/colorPalette.ts
@@ -0,0 +1,104 @@
+export const textLight = {
+  body: "#1A1A1C",
+  description: "#626264",
+  placeHolder: "#757578",
+  onFill: "#FFFFFF",
+  link: "#0017C1",
+  hover: "#000082",
+  active: "#000082",
+  visited: "#000082",
+  alert: "#EC0000",
+  disabled: "#949497",
+};
+export const textDark = {
+  body: "#FFFFFF",
+  description: "#B4B4B7",
+  placeHolder: "#949497",
+  onFill: "#FFFFFF",
+  link: "#7096F8",
+  hover: "#9DB7F9",
+  active: "#9DB7F9",
+  visited: "#9DB7F9",
+  alert: "#FF4B36",
+  disabled: "#757578",
+};
+
+export const iconLight = {
+  label: "#1A1A1C",
+  active: "#0017C1",
+  alert: "#EC0000",
+  disabled: "#949497",
+};
+
+export const iconDark = {
+  label: "#FFFFFF",
+  active: "#7096F8",
+  alert: "#FF4B36",
+  disabled: "#757578",
+};
+
+export const buttonLight = {
+  normal: "#0017C1",
+  hover: "#000082",
+  active: "#000082",
+  disabled: "#949497",
+};
+
+export const buttonDark = {
+  normal: "#7096F8",
+  hover: "#4979F5",
+  active: "#4979F5",
+  disabled: "#949497",
+};
+
+export const backgroundLight = {
+  primary: "#FFFFFF",
+  secondary: "#F1F1F4",
+  tertiary: "#F8F8FB",
+};
+
+export const backgroundDark = {
+  primary: "#1A1A1C",
+  secondary: "#626264",
+  tertiary: "#414143",
+};
+
+export const borderLight = {
+  field: "#1A1A1C",
+  divider: "#D8D8DB",
+  focused: "#D18D0F",
+  selected: "#0017C1",
+  alert: "#EC0000",
+  disabled: "#949497",
+};
+
+export const borderDark = {
+  field: "#FFFFFF",
+  divider: "#626264",
+  focused: "#D18D0F",
+  selected: "#7096F8",
+  alert: "#FF4B36",
+  disabled: "#949497",
+};
+
+export const statusLight = {
+  success: "#259D63",
+  alert: "#EC0000",
+  warning: "#C16800",
+};
+
+export const statusDark = {
+  success: "#259D63",
+  alert: "#FF4B36",
+  warning: "#D18D0F",
+};
+
+export const chartLight = {
+  primary: "#0017C1",
+  secondary: "#4979F5",
+};
+
+export const chartDark = {
+  primary: "#C5D7FB",
+  secondary: "#9DB7F9",
+};
